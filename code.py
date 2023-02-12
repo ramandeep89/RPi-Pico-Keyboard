955e614f5d95f4627ede42c3e84de19ef82f455b
Ramandeep Singh <ramandeep89@gmail.com>
2023-02-12 17:39:54 +0530

    Added profiles to board

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 # RaspberryPi Pico Macroboard
 
 import time
+import asyncio
 import board
 from digitalio import DigitalInOut, Direction, Pull
 import usb_hid
@@ -13,9 +14,19 @@ from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 print("---Pico Pad Keyboard---")
 
+profile = 0
+
 led = DigitalInOut(board.LED)
 led.direction = Direction.OUTPUT
-#led.value = True
+async def blinkLED():
+    while True:
+        led.value = True
+        await asyncio.sleep(0.05)
+        led.value = False
+        await asyncio.sleep(1/(profile+1))
+
+
+led.value = profile
 
 kbd = Keyboard(usb_hid.devices)
 cc = ConsumerControl(usb_hid.devices)
@@ -36,6 +47,7 @@ pins = (
     board.GP18,
 )
 
+CHPRFL = -1 # (CHPRFL,) make sure to have comma before tuple end and each profile have atleast one CHPRFL keymap
 SLEEP = 0 # (SLEEP, 1)
 MEDIA = 1 # (MEDIA, ConsumerControlCode.MUTE)
 KEY = 2 # (KEY, [Keycode.X]) OR (KEY, [Keycode.LEFT_CONTROL, Keycode.X])
@@ -43,22 +55,54 @@ TYPE = 3 # (TYPE, "Ramandeep Singh")
 RUN = 4 # (RUN, "wt.exe")
 MACRO = 5 # (MACRO, [(RUN, "wt.exe"), (SLEEP, 1), (TYPE, "ssh root@192.168.29.2")])
 
-# board config according to schematics
+# board config keymap according to schematics
 #      9
 # 2 5 8
 # 1 4 7
 # 0 3 6
+# board config encodermap according to schematics
+# >> 0
+# << 1
+
+# keymap/encodermap is in format {(profile1): {(key1) : (MACRO),...}, ...}
+
 keymap = {
-    (0): (KEY, [Keycode.LEFT_CONTROL, Keycode.X]),
-    (1): (TYPE, "Ramandeep Singh"),
-    (2): (KEY, [Keycode.LEFT_CONTROL, Keycode.Z]),
-    (3): (KEY, [Keycode.LEFT_CONTROL, Keycode.C]),
-    (4): (TYPE, "ramandeep89@gmail.com"),
-    (5): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.Z]),
-    (6): (KEY, [Keycode.LEFT_CONTROL, Keycode.V]),
-    (7): (TYPE, "p3w.pew@yandex.com"),
-    (8): (RUN, "wt.exe"),
-    (9): (MEDIA, ConsumerControlCode.MUTE),
+    (0): {
+        (0): (KEY, [Keycode.LEFT_CONTROL, Keycode.X]),
+        (1): (TYPE, "Ramandeep Singh"),
+        (2): (KEY, [Keycode.LEFT_CONTROL, Keycode.Z]),
+        (3): (KEY, [Keycode.LEFT_CONTROL, Keycode.C]),
+        (4): (TYPE, "ramandeep89@gmail.com"),
+        (5): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.Z]),
+        (6): (KEY, [Keycode.LEFT_CONTROL, Keycode.V]),
+        (7): (TYPE, "p3w.pew@yandex.com"),
+        (8): (RUN, "wt.exe"),
+        (9): (CHPRFL,),
+    },
+    (1): {
+        (0): (KEY, [Keycode.LEFT_CONTROL, Keycode.X]),
+        (1): (TYPE, "Ramandeep Singh"),
+        (2): (KEY, [Keycode.LEFT_CONTROL, Keycode.Z]),
+        (3): (KEY, [Keycode.LEFT_CONTROL, Keycode.C]),
+        (4): (TYPE, "ramandeep89@gmail.com"),
+        (5): (KEY, [Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.Z]),
+        (6): (KEY, [Keycode.LEFT_CONTROL, Keycode.V]),
+        (7): (TYPE, "p3w.pew@yandex.com"),
+        (8): (MACRO, [(RUN, "wt.exe"), (SLEEP, 1), (TYPE, "ssh root@192.168.29.2")]),
+        (9): (CHPRFL,),
+    },
+}
+
+encoder = rotaryio.IncrementalEncoder(board.GP16, board.GP17)
+encodermap = {
+    (0): {
+        (0): (MEDIA, ConsumerControlCode.VOLUME_INCREMENT),
+        (1): (MEDIA, ConsumerControlCode.VOLUME_DECREMENT),
+    },
+    (1): {
+        (0): (MEDIA, ConsumerControlCode.VOLUME_INCREMENT),
+        (1): (MEDIA, ConsumerControlCode.VOLUME_DECREMENT),
+    },
 }
 
 switches = []
@@ -71,13 +115,16 @@ for i in range(len(pins)):
 
 switch_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-encoder = rotaryio.IncrementalEncoder(board.GP16, board.GP17)
-last_position = encoder.position
+
 
 def keypress(key):
-    if key[0] == SLEEP:
+    global profile
+    if key[0] == CHPRFL:
+        profile = (profile+1) % len(keymap)
+        led.value = profile % 2
+    elif key[0] == SLEEP:
         time.sleep(key[1])
-    if key[0] == KEY:
+    elif key[0] == KEY:
         kbd.send(*key[1])                        
     elif key[0] == MEDIA:
         cc.send(key[1])
@@ -95,35 +142,36 @@ def keypress(key):
     else:
         print("unknown operation")
 
-while True:
-    
-    current_position = encoder.position
-    position_change = current_position - last_position
-    if position_change > 0:
-        for _ in range(position_change):
-            cc.send(ConsumerControlCode.VOLUME_INCREMENT)
-        print(current_position)
-    elif position_change < 0:
-        for _ in range(-position_change):
-            cc.send(ConsumerControlCode.VOLUME_DECREMENT)
-        print(current_position)
-    last_position = current_position    
-    
-    for button in range(10):
-        if not switch_state[button]:
-            if not switches[button].value:
-                try:
-                    keypress(keymap[button])
-                except ValueError:  # deals w six key limit
-                    pass
-                switch_state[button] = 1
 
-        if switch_state[button]:
-            if switches[button].value:
-                switch_state[button] = 0
+async def main():
+    last_position = encoder.position
+    while True:
+        current_position = encoder.position
+        position_change = current_position - last_position
+        if position_change > 0:
+            for _ in range(position_change):
+                keypress(encodermap[profile][0])
+            print(current_position)
+        elif position_change < 0:
+            for _ in range(-position_change):
+                keypress(encodermap[profile][1])
+            print(current_position)
+        last_position = current_position    
+        
+        for button in range(10):
+            if not switch_state[button]:
+                if not switches[button].value:
+                    try:
+                        keypress(keymap[profile][button])
+                    except ValueError:  # deals w six key limit
+                        pass
+                    switch_state[button] = 1
 
-    time.sleep(0.01)  # debounce
-    
+            if switch_state[button]:
+                if switches[button].value:
+                    switch_state[button] = 0
 
+        time.sleep(0.01)  # debounce
+        
 
-
+asyncio.run(main())
